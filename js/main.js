e9aca8c67df53aa67261b200b5fda52d479caaea
Bentleyr98 <bentley.rebekah314@gmail.com>
2022-09-17 23:41:46 -0600

    Initial build of my portfolio

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const links = [
+    {
+      label: "Week 1 notes",
+      url: "week1/index.html"
+    },
+    {
+        label: "Week 2 notes",
+        url: "week2/index.html"
+    }
+  ]
+
+  let currentdate = document.lastModified;
+  let date = new Date();
+  let year = date.getFullYear();
+  document.querySelector('.date').textContent = currentdate;
+  document.querySelector('.year').textContent = year;
+
+
+
+const div = document.querySelector(".links");
+const ol = document.createElement('ol');
+
+div.appendChild(ol);
+
+
+links.forEach(link => {
+    document.querySelector('ol').innerHTML += `<li><a href=${link.url}> ${link.label}</a> </li>`;
+})
+
+
