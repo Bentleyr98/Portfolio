1725a57653243fd6ab2eff4b3589e5577f50a474
Bentleyr98 <bentley.rebekah314@gmail.com>
2022-11-22 19:48:09 -0700

    Fixed generic styling for assignments

---
+++ js/default.js
@@ -0,0 +1,7 @@
+let currentdate = document.lastModified;
+  let date = new Date();
+  let year = date.getFullYear();
+  document.querySelector('.date').textContent = currentdate;
+  document.querySelector('.year').textContent = year;
+
+
