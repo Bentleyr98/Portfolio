f77d332c814522e6297e96ce74e47efebbd25596
Bentleyr98 <bentley.rebekah314@gmail.com>
2022-12-15 01:39:05 -0700

    add a filter to ls

--- week6/scripts/ls.js
+++ week6/scripts/ls.js
@@ -4,8 +4,10 @@ function ls (){
     let items = Array();
     let keys = Object.keys(localStorage);
     for(let key of keys) {
+        let obj = JSON.parse(localStorage.getItem(key))
+        if(obj.text){
         items.push(JSON.parse(localStorage.getItem(key)));
-        }
+        }}
 
     return items;
 }
